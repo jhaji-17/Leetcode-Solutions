4e4915474f17e149b690ecf44a3e54273e83b289
jhaji-17 <jhaji1705@gmail.com>
2026-06-26 00:17:25 +0530

    solved remove duplicate optimally

---
+++ array/003_remove_duplicate_optimized.cpp
@@ -0,0 +1,88 @@
+/**
+ *    author:  Abhinav Kumar Jha
+ *    created: 25.06.2026 18:12:26
+**/
+
+#include <bits/stdc++.h>
+
+// --- For Debugging & Failcheck ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#include <unistd.h> // Required for alarm()
+#else
+#define debug(...) 42
+#endif
+// -------------------
+
+using namespace std;
+
+#define ll long long int
+#define endl '\n'
+
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+
+// --- Main Solve Function ---
+void solve() {
+    int n; cin>>n;
+    vector<int> v(n); for(int i =0;i<n; i++) cin>>v[i];ll j=1,temp=1;
+    for (int i = 0; i < n; i++)
+    {
+        if(j==n) break;
+        if(v[j]==v[i])
+        {
+            j++;
+            continue;
+        }
+        else
+        {
+            v[temp]=v[j];
+            i=j-1;
+            j++;
+            temp++;
+        }
+
+        
+    }
+    debug(temp);
+    
+}
+
+// --- Main Function ---
+int32_t main() {
+    #ifdef LOCAL
+    alarm(3); // Failcheck: Kills program after 3 seconds locally to prevent OS crashes
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
