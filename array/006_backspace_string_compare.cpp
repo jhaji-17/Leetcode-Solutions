428a8dd1b272b8431f61ae083ac91ab54d1cf51b
jhaji-17 <jhaji1705@gmail.com>
2026-07-05 20:11:31 +0530

    add backspace string compare

---
+++ array/006_backspace_string_compare.cpp
@@ -0,0 +1,126 @@
+/**
+ *    author:  Abhinav Kumar Jha
+ *    created: 05.07.2026 19:19:25
+**/
+
+#include <bits/stdc++.h>
+
+// --- For Debugging & Failcheck ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#include <unistd.h> // Required for alarm()
+#else
+#define debug(...) 42
+#endif
+// -------------------
+
+using namespace std;
+
+#define ll long long int
+#define endl '\n'
+
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+
+// --- Main Solve Function ---
+void solve() {
+    string s,t,tmp1,tmp2;
+    cin>>s>>t;
+    ll i = s.size()-1,skip=0;
+    while (i>=0)
+    {
+        if(s[i]=='#')
+        {
+            skip++;
+            i--;
+        }
+        else
+        {
+            if(skip==0)
+            {
+                tmp1.push_back(s[i]);
+                i--;
+            }
+            else
+            {
+                skip--;
+                i--;
+            }
+        }
+    }
+    debug(tmp1);
+
+    i=t.size()-1;
+    skip=0;
+    while (i>=0)
+    {
+        if(t[i]=='#')
+        {
+            skip++;
+            i--;
+        }
+        else
+        {
+            if(skip==0)
+            {
+                tmp2.push_back(t[i]);
+                i--;
+            }
+            else
+            {
+                i--;
+                skip--;
+            }
+        }
+    }
+    debug(tmp1);
+    debug(tmp2);
+    if(tmp1==tmp2)
+    {
+        cout<<true;
+    }
+    else
+    {
+        cout<<false;
+    }
+    
+}
+
+// --- Main Function ---
+int32_t main() {
+    #ifdef LOCAL
+    alarm(3); // Failcheck: Kills program after 3 seconds locally to prevent OS crashes
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
