7313f744a92fe196a388463ac44c7212316dfab8
jhaji-17 <jhaji1705@gmail.com>
2026-07-13 22:43:41 +0530

    solved subarray product less than k

---
+++ array/013_ Subarray Product Less Than K.cpp
@@ -0,0 +1,82 @@
+/**
+ *    author:  Abhinav Kumar Jha
+ *    created: 13.07.2026 22:18:40
+**/
+
+#include <bits/stdc++.h>
+
+// --- For Debugging & Failcheck ---
+#ifdef LOCAL
+#include "algo/debug.h"
+#include <unistd.h> // Required for alarm()
+#else
+#define debug(...) 42
+#endif
+// -------------------
+
+using namespace std;
+
+#define ll long long int
+#define endl '\n'
+
+// --- Type Definitions ---
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vp;
+
+// --- Shorthand Macros ---
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define pb push_back
+
+// --- I/O Helper Functions ---
+void vin(vi &a, ll n) {
+    a.resize(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+}
+
+void pin(const vi &a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        cout << a[i] << (i == a.size() - 1 ? "" : " ");
+    }
+    cout << endl;
+}
+
+// --- Main Solve Function ---
+void solve() {
+    ll n,k;cin>>n>>k;
+    vi v; vin(v,n);
+        ll left =0,mul=1,count=0;
+        for (int i = 0; i < n; i++)
+        {
+            mul = v[i]*mul;
+            while (mul>=k&&left<=i)
+            {
+                mul=mul/v[left];
+                left++;
+            }
+            count+=i-left+1;
+            
+        }
+        cout<<count;
+
+}
+
+// --- Main Function ---
+int32_t main() {
+    #ifdef LOCAL
+    alarm(3); // Failcheck: Kills program after 3 seconds locally to prevent OS crashes
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
